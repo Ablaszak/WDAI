46f43dd908c5eb80b2493c5a6614399b9ebb1d7e
Ablaszak <a.t.blasiak@gmail.com>
2025-11-25 12:30:36 +0100

    finished stopwatch

---
+++ lab3/stoper.js
@@ -0,0 +1,40 @@
+let sec = 0;
+let mins = 0;
+let go = false;
+
+function timer(){
+    if(go) {
+        if (sec < 59)
+            sec++
+        else {
+            mins++;
+            sec = 0;
+        }
+
+        update();
+
+        setTimeout(timer, 1000);
+    }
+}
+
+function start(){
+    if(!go) {
+        go = true;
+        timer();
+    }
+}
+function stop(){
+    go = false;
+}
+
+function reset(){
+    sec=0;
+    mins=0;
+    update();
+}
+
+function update(){
+    document.getElementById("seconds").innerHTML = sec + "s";
+    if (mins > 0)
+        document.getElementById("minutes").innerHTML = mins + "min ";
+}
