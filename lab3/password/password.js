dd3847c3c2361e569fae29b2e9f3916f10aadde6
Ablaszak <a.t.blasiak@gmail.com>
2025-11-27 11:12:48 +0100

    fixed password, the issue was java script reading number input as string (wtf)

--- lab3/password/password.js
+++ lab3/password/password.js
@@ -1,31 +1,20 @@
 let len = 0;
-let set = [];
+let set = "";
 
 function gen_len(){
-    let l = document.getElementById("minlength");
-    let r = document.getElementById("maxlength");
-    len = Math.floor(Math.random() * (r + 1 - l)) + l;
+    let l = parseInt(document.getElementById("minlength").value);
+    let r = parseInt(document.getElementById("maxlength").value);
+    len = Math.floor(Math.random() * (r - l + 1)) + l;
 }
 
 function gen_set(){
-
+    set= "qwertyuiopasdfghjklzxcvbnm";
     if(document.getElementById("special").checked)
-    {
-        for(let i = 33; i<65; i++)
-            set.push(String.fromCharCode(i));
-        for(let i = 91; i<97; i++)
-            set.push(String.fromCharCode(i));
-        for(let i = 173; i<177; i++)
-            set.push(String.fromCharCode(i));
-    }
+        set = set + "!@#$%^&*()_-+=[{]};:',<.>/?";
 
     if(document.getElementById("big").checked)
-        for(let i = 65; i<91; i++)
-            set.push(String.fromCharCode(i));
+        set = set + "QWERTYUIOPASDFGHJKLZXCVBNM";
 
-
-    for(let i = 97; i<173; i++)
-        set.push(String.fromCharCode(i));
 }
 
 function gen_password(){
