9d579d14013ddbef35c833bcff3fcdac82d77f76
Ablaszak <a.t.blasiak@gmail.com>
2025-11-28 19:51:27 +0100

    wip

--- flappy_bird/flappy.js
+++ flappy_bird/flappy.js
@@ -1,15 +1,44 @@
+const game = document.querySelector(".The_Game");
 
 function run(){
 
+    function move(){
+
+        let pipes = game.querySelectorAll(".pipe");
+
+        for(let pipe of pipes){
+            if(pipe.getBoundingClientRect().right < 0)
+                pipe.remove();
+            else{
+                let position = parseInt(pipe.style.left);
+                pipe.style.left = (position - 3) + 'px';
+            }
+        }
+        requestAnimationFrame(move);
+    }
     let pipeXdist = 0;
     function spawnPipe(){
 
-        if(pipeXdist < 150)
-            pipeXdist++;
-        else{
-            let pipeYdist
+        if(pipeXdist > 150)
+        {
+            pipeXdist = 0;
+            let pipeYdist = 10;
+            let pipe = document.createElement("div");
+            pipe.className = "pipe";
+            pipe.style.bottom = 0 + "vh";
+            pipe.style.left = game.offsetWidth + "px";
+
+            game.append(pipe);
         }
+        pipeXdist ++;
+        requestAnimationFrame(spawnPipe);
 
     }
 
+
+    requestAnimationFrame(spawnPipe);
+    requestAnimationFrame(move);
+
 }
+
+run();
