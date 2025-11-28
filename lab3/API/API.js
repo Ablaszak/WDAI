4e54b115ca70c8693b851116105aac7aa98f1fc8
Ablaszak <a.t.blasiak@gmail.com>
2025-11-28 13:00:33 +0100

    finished, might improve comparison function

--- lab3/API/API.js
+++ lab3/API/API.js
@@ -15,6 +15,11 @@ async function load(){
 
 function show(){
 
+    let filter_phrase = String(document.getElementById("filter").value).toLowerCase();
+    if(filter_phrase === "Filtruj")
+        filter_phrase = "";
+
+
     let table = document.getElementById("table");
 
     table.innerHTML = `
@@ -24,16 +29,39 @@ function show(){
             <th>Opis</th>
         </tr>
     `;
+    let tab = data.products.slice();
+    sort(tab);
 
-    for(const row of data.products){
-        table.innerHTML += `
-        <tr>
-            <td><img width="100px" src="${row.images[0]}" alt="product image"></td>
-            <td>${row.title}</td>
-            <td>${row.description}</td>
-        </tr>
-    `;
+    for(const row of tab){
+        if((row.title).toLowerCase().includes(filter_phrase))
+        {
+                table.innerHTML += `
+            <tr>
+                <td><img width="100px" src="${row.images[0]}" alt="product image"></td>
+                <td>${row.title}</td>
+                <td>${row.description}</td>
+            </tr>
+        `;
+        }
     }
 }
+
+function sort(tab){
+    let sortby = document.getElementById("sort").value;
+    switch(sortby){
+        case("asc"):
+            tab.sort();
+            break;
+        case("desc"):
+            tab.sort().reverse();
+            break;
+    }
+}
+
+
+let sortinput = document.getElementById("sort");
+sortinput.addEventListener('input', show)
+let filterinput = document.getElementById("filter");
+filterinput.addEventListener('input', show)
 
 load();
