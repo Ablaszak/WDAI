c8592b8e24d9546014af69db96465a8d6fb89f1d
Ablaszak <a.t.blasiak@gmail.com>
2025-11-28 17:59:24 +0100

    added assets

---
+++ flappy_bird/flappy.js
@@ -0,0 +1,15 @@
+
+function run(){
+
+    let pipeXdist = 0;
+    function spawnPipe(){
+
+        if(pipeXdist < 150)
+            pipeXdist++;
+        else{
+            let pipeYdist
+        }
+
+    }
+
+}
