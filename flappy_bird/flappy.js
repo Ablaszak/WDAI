d675cd128c503a31fbe8a201b5b31b44f2575d64
Ablaszak <a.t.blasiak@gmail.com>
2025-11-29 13:53:53 +0100

    added comments

--- flappy_bird/flappy.js
+++ flappy_bird/flappy.js
@@ -5,6 +5,7 @@ function run(){
 
     function move(){
 
+        // delete and move pipes
         let pipes = game.querySelectorAll(".pipe");
 
         for(let pipe of pipes){
@@ -20,13 +21,16 @@ function run(){
     let pipeXdist = 0;
     function spawnPipe(){
 
+        //spawn pipes if there's enough space between them
         if(pipeXdist > 75)
         {
             pipeXdist = 0;
 
+            // generate obstacle height and gap
             let pipeYdist = (Math.random() * 15) + 5; // (5 , 20)
             let mid = (Math.random() * 20) + 40; // (40, 60);
 
+            //actually create pipes, just don't touch the numbers pls
             let pipe = document.createElement("div");
             pipe.className = "pipe";
             pipe.style.top = (mid + pipeYdist) + "vh";
